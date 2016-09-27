54b04e321449a773f3f0c0aca263f166c67853a1
Johnny <johnny@Admin-PK-MBP.local>
2016-09-28 00:32:05 +0900

    Initial Commit

---
+++ busping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  busping
+//
+//  Created by Johnny on 2016/09/28.
+//  Copyright © 2016 Johnny Linnert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
