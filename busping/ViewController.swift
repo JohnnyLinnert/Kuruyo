97e632ea337c00913612effc82f8cccf3e574aad
Johnny Linnert <johnny@Admin-PK-MBP.local>
2016-10-09 15:35:09 +0900

    Progress so far
    Integrated Alamofire and Kanna, Alamofire is outputting HTML of the
    target URL

--- busping/ViewController.swift
+++ busping/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import Alamofire
+import Kanna
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UITableViewDelegate {
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+
+
+        Alamofire.request("http://tokyu.bus-location.jp/blsys/navi?VID=rtl&EID=nt&PRM=&RAMK=116&SCT=1").response { response in
+            print("Request: \(response.request)")
+            print("Response: \(response.response)")
+            print("Error: \(response.data)")
+
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)")
+            }
+
+        }
+
+
+
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+                
+
+        }
+
     }
-
-
-}
 
