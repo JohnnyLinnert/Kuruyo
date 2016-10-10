a104863e8c778c21cb72242cd988fcd3a5d69e12
Johnny Linnert <johnny@Admin-PK-MBP.local>
2016-10-10 20:05:09 +0900

    Added Scraping and Parsing
    Moved the structure around a bit, and added some functions thanks to
    the help of Jonathon. Parsing is ready to go, just need to find the
    best way to pull out relevant data.

--- busping/ViewController.swift
+++ busping/ViewController.swift
@@ -18,7 +18,11 @@ class ViewController: UIViewController, UITableViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+         getBusInfo()
 
+    }
+
+    func getBusInfo () -> Void {
 
         Alamofire.request("http://tokyu.bus-location.jp/blsys/navi?VID=rtl&EID=nt&PRM=&RAMK=116&SCT=1").response { response in
             print("Request: \(response.request)")
@@ -26,14 +30,26 @@ class ViewController: UIViewController, UITableViewDelegate {
             print("Error: \(response.data)")
 
             if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-                print("Data: \(utf8Text)")
+                print(utf8Text)
+                self.cleanBusInfo(html: utf8Text)
             }
-
         }
+    }
 
+    func cleanBusInfo (html: String) -> Void {
+        if let doc = Kanna.HTML(html: html, encoding: String.Encoding.utf8) {
 
+//            for busIcon in doc.css("td.balloonL > dl > dt > img") {
+//                print(busIcon["src"])
+//            }
+//            for busStop in doc.css("td.stopName > a") {
+//                print(busStop.text)
+//            }
 
-
+            for busStop in doc.css("tr") {
+                print(busStop.className)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
