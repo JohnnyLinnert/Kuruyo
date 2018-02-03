a072f287e9952ba99d1695c300560edfa863803f
Johnny <jlinnertii@gmail.com>
2018-02-03 19:35:35 +0900

    Adding Fastlane
    So we can allow the app to be beta-tested using TestFlight

---
+++ fastlane/Appfile.swift
@@ -0,0 +1,9 @@
+var appIdentifier: String { return "com.johnnylinnert.busping" } // The bundle identifier of your app
+var appleID: String { return "jwlii@mac.com" } // Your Apple email address
+
+var itcTeam: String? { return "2159671" } // iTunes Connect Team ID
+var teamID: String { return "7K9KLB5QXN" } // Apple Developer Portal Team ID
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
